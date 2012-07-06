ebc6c8618bb81efb07c0e97657a0807b8e5336a1
richardp <swampopus@gmail.com>
2012-07-06 10:20:38 -0500

    Started update_status module, as well as working to refine the sample module which comes with FlightPath.

--- flightpath/cron.php
+++ flightpath/cron.php
@@ -1,58 +1,27 @@
 <?php
  
-
-// We need to discover what the "levelsDeep" might be, based on the arguments
-// used to run cron.php.
-// Ex:  If it was run with:
-//   php flightpath/cron.php
-// then the levelDeep = "flightpath/"
-
-$temp = trim($argv[0]);
-$levels_deep = trim(str_replace("cron.php", "", $temp));
-
+/**
+ * @file
+ * The cron.php file for FlightPath, which should be run periodically.
+ * 
+ * This file will invoke hook_cron on all available modules.  It should be
+ * accessed in a similar method as 
+ * wget http://url/cron.php?t=CRON_TOKEN
+ * 
+ * You can find your site's cron token (and change it if you wish)
+ * in your /custom/settings.php file.
+ */
+ 
 require_once("bootstrap.inc");
 
-if ($GLOBALS["fp_system_settings"]["cron_security_token"] == "") {
-  print "\n\n!! A security token has not been set up in the custom/settings.php file.";
-  print "\n!! This is required in order to run cron.php";
-  print "\n\n";
-  die;
+$token = $_REQUEST["t"];
+if ($token != $GLOBALS["fp_system_settings"]["cron_security_token"]) {
+  //die("Sorry, cron security token does not match.");
 }
 
-if ($GLOBALS["fp_system_settings"]["cron_security_token"] != "" 
-    && $argv[1] != $GLOBALS["fp_system_settings"]["cron_security_token"]) {
-  print "\n\nSecurity token is invalid or not supplied.\n\n";
-  print "   Usage: php cron.php security_token_goes_here \n\n";
-  die;
-}
+watchdog("cron", "Cron run started", array(), WATCHDOG_NOTICE);
+invoke_hook("cron");
+watchdog("cron", "Cron run completed", array(), WATCHDOG_NOTICE);
 
-// If we are here, it means that we have been granted access to continue
-// running the cron.  We will set up a GLOBALS variable which will let us
-// verify in any other scripts we run that were have been granted access (optional)
-$GLOBALS["fp_cron_granted_access"] = $argv[1];
-
-
-// Cron is responsible for pruning the log table.  Execute the query now
-// to do that.
-$db = get_global_database_handler();
-$older_than_days = $GLOBALS["fp_system_settings"]["cron_delete_logs_older_than_days"];
-if ($older_than_days != "" && is_numeric($older_than_days)) {
-  if ($older_than_days < 30) $older_than_days = 30;
-  $db->db_query("DELETE FROM log WHERE datetime < (NOW() - INTERVAL $older_than_days DAY)");
-}
-
-// Okay, we will now go through all of our modules and call their hook_cron()
-// method, if one exists.
-foreach ($GLOBALS["fp_system_settings"]["modules"] as $module => $value) {
-  if (isset($value["disabled"]) && $value["disabled"] == "yes") {
-    // Module is not enabled.  Skip it.
-    continue;
-  }
-
-  if (function_exists($module . "_cron")) {
-    call_user_func($module . "_cron");
-  }
-  
-}
 
 ?>
