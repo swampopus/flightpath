b84d9d857ebdaa5349c6969f6d0c673dfb73a0f9
Richard Peacock <swampopus@gmail.com>
2014-07-30 13:34:38 -0500

    2014-07-30   Added a new Batch API for processing large amounts of data without a timeout.  Modified system.install and system.module to work with the new Batch module.
      Minor changes and corrections in other files.
    
      Added a new Student Progress Report to the Stats module, which takes advantage of the new Batch API, due to it taking such
      a long time to run.
    
    2014-06-23
      Modified the Stats module, so that its main menu page simply displays any menu item that begins
      with the path "stats/reports".  This allows users to create new reports in custom modules, and the
      links will still appear on that page.  Ex:  stats/reports/my-custom-report.  Just make sure that
      the "type" => MENU_TYPE_NORMAL_ITEM and the url begins with "stats/reports".  And remember to clear
      cache after creating any new menu items.

---
+++ flightpath/modules/batch/README.txt
@@ -0,0 +1,18 @@
+README File for Batch API module
+================================
+
+For instructions on this module's use (which appeals primarily to developers), search
+FlightPath's documentation: http://getflightpath.com/node/1121
+
+
+
+
+====================
+Testing and Examples
+====================
+
+To demonstrate how the Batch process works, a test has been created as an example.
+
+As admin, visit example.com/batch-test-form to see it in action.
+
+View the source code file batch.test.inc for the documented working example.
---
+++ flightpath/modules/batch/js/batch.js
@@ -0,0 +1,76 @@
+/**
+ * @file
+ * Main javascript file for Batch module.
+*/
+
+
+// When the page has finished loading, let's start the batch process up...
+$(document).ready(function() {
+  
+
+  //activate the ajax function...
+  batchContinueBatch();
+   
+  
+});
+
+
+
+// Continue the batch process...
+function batchContinueBatch() {
+  var batchId = FlightPath.settings.batch_id;
+  var basePath = FlightPath.settings.basePath;
+  
+  // Contact via Ajax...
+  $.get(basePath + "/batch-ajax-callback/" + batchId, function(data) {
+
+    // Handle data. 
+    if (data.error != null) {
+      alert(data.error);
+      return;
+    }
+    
+    if (data.success != "SUCCESS") {
+      // We got some kind of error!
+      alert("Error processing batch.  Returned 'data' message: \n\n" + data);
+      return;
+    }
+    
+    // No error. Let's update our values...
+    var progressMsg = data.progress_message;
+    var percent = data.percent;
+        
+    
+    // Update the screen.
+    $("#batch-progress-message").html("<div>" + progressMsg + "</div>");
+    // Update the width of the progress bar.
+    $("#batch-progress-bar").css("width", percent + "%");
+    
+    // Add percent to the progress bar?
+    if (data.display_percent == true) {
+      $("#batch-progress-bar").html("<span class='percent'>" + percent + "%</span>");
+    }
+    
+    
+    // If we are finished, then we can proceed to our finished handler.
+    if (data.finished == "finished") {
+      // Redirect to our finished page...
+      window.location = basePath + "/batch-finished/" + batchId;
+      return;
+    }
+    
+    
+    // We now need to go back through the loop.  Call this function after a brief delay second...
+    setTimeout(batchContinueBatch(), 100);
+    
+    
+    
+  });
+  
+  
+  
+  
+  
+  
+  
+}
