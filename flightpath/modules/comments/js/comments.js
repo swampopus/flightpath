be0746d9a4a81a6b7af5cc9330e3d0588f006c93
Richard Peacock <swampopus@gmail.com>
2018-03-11 22:37:33 -0500

    Fixed a non-clean URL issue in comments.js, where deletion was not possible unless clean URLs was enabled.

--- flightpath/modules/comments/js/comments.js
+++ flightpath/modules/comments/js/comments.js
@@ -6,6 +6,11 @@
 function deleteComment(id) {
   var x = confirm("Are you sure you wish to delete this comment?\nThis action cannot be undone.");
   if (x) {
-    window.location=FlightPath.settings.basePath + "/comments/delete-comment&current_student_id=" + FlightPath.settings.currentStudentId + "&comment_id=" + id;
+    //window.location=FlightPath.settings.basePath + "/comments/delete-comment&current_student_id=" + FlightPath.settings.currentStudentId + "&comment_id=" + id;
+    
+    // To make compatible with non-clean URL sites, we will use the "unclean" URL...
+    var url = FlightPath.settings.basePath + "/index.php?q=" + "comments/delete-comment&current_student_id=" + FlightPath.settings.currentStudentId + "&comment_id=" + id;;
+    window.location = url;
+    
   }
 } 
