385046cdbbd82396ae286fddb246e666110dea04
Richard Peacock <swampopus@gmail.com>
2019-06-07 22:38:19 -0500

    sdfsdf

--- flightpath/classes/_DatabaseHandler.php
+++ flightpath/classes/_DatabaseHandler.php
@@ -43,6 +43,7 @@ class _DatabaseHandler extends stdClass
       $this->pdo = new PDO("mysql:host=$db_host_ip;port=$db_port;dbname=$db_name;charset=utf8", $db_user, $db_pass);
       // Set our error handling...  (using "silent" so I can catch errors in try/catch and display them, email, etc, if wanted.)
       $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, TRUE);
            
     }
     
