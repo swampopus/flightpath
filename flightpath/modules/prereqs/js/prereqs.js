81749417bf2c2d6ad48d1c205ff8e4859881abb1
Richard Peacock <swampopus@gmail.com>
2022-04-15 09:52:24 -0500

    Added extra permission to prereqs module, so that you can only override locks on courses if you have permission.
      Adjusted the "locked but checked" icon for courses in prereqs, to make the check a little more obvious.

--- flightpath/modules/prereqs/js/prereqs.js
+++ flightpath/modules/prereqs/js/prereqs.js
@@ -3,3 +3,19 @@
  * javascript file for prereqs module
 */
 
+
+/**
+ * The user is not allowed to toggle the selection.
+ *  
+ */
+function prereqs_no_toggleSelection(uniqueID, display_status, warningMsg) {
+  fp_alert(warningMsg);
+  return false;
+}
+
+
+/* user cannot toggle selection from group */
+function prereqs_no_group_toggleSelection(warningMsg) {
+  fp_alert(warningMsg);
+  return false;
+}
